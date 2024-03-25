aa26e7d3907522693e7c6fe66e54f811377123f4
chaeyoo <chaeh0124@gmail.com>
2024-03-25 20:01:56 +0900

    [FEAT] carousel 기능 추가

--- carousel/main.js
+++ carousel/main.js
@@ -1,58 +1,145 @@
+const iconPrev = `<div style="width: 40px; height: 20px; background-color: red; cursor: pointer">이전</div>`;
+
+const iconNext = `<div style="width: 40px; height: 20px; background-color: red; cursor: pointer">다음</div>`
+
 const app = createEl({
-    tagName: "div",
-    parent: document.body
+    tagName: 'div',
+    parent: document.body,
 });
-app.style.cssText = `width: 700px;
-height: 250px;
-background-color: rgb(100, 100, 100);
-`
+
+app.style.cssText = `
+    width: 400px;
+    height: 400px;
+    background-color: gray;
+`;
 
 const wrapper = createEl({
-    tagName: "div",
-    parent: app
-})
+    tagName: 'div',
+    parent: app,
+});
 wrapper.style.cssText = `
     width: 100%;
     height: 100%;
     position: relative;
     display: flex;
+    overflow: hidden
 `;
 
-const [prevButton, nextButton] = createEl({
-    tagName: 'button',
+
+const itemContainer = createEl({
+    tagName: 'div',
     parent: wrapper,
-    count: 2
 });
 
-prevButton.style.cssText = `
-    position: absolute;
-    z-index:1;
-    border: 0;
-    top: 0;
-    width: 50px;
-    height: 100%;
-    background-color: transparent;
-    background: linear-gradient(90deg, rgba(0, 0, 0, 0.3) 0%, rgba(0, 0, 0, 0.1) 30%, rgba(0, 0, 0, 0) 100%);
-    filter: drop-shadow(3px 5px 2px rgba(0 0 0 / 0.7));
-`
+itemContainer.style.cssText = `
+    display: flex;
+    transform: translateX(-400px);
+`;
+addBtns();
 
-nextButton.style.cssText = `
+addImgItem(itemContainer, './images/01.jpg');
+addImgItem(itemContainer, './images/02.jpg');
+addImgItem(itemContainer, './images/03.jpg');
+addImgItem(itemContainer, './images/04.jpg');
+addImgItem(itemContainer, './images/05.jpg');
+
+
+function handleSlide(next = true) {
+    itemContainer.style.transitionDuration = '0.5s';
+    itemContainer.style.transform = `translateX(${next ? -800 : 0}px)`;
+    itemContainer.ontransitionend = () => {
+        itemContainer.style.removeProperty('transition-duration');
+        itemContainer.style.transform = 'translateX(-400px)';
+        next ? itemContainer.appendChild(itemContainer.firstChild)
+            : itemContainer.prepend(itemContainer.lastChild);
+    }
+}
+
+
+function addBtns() {
+
+    const [prevBtn, nextBtn] = createEl({
+        tagName: 'button',
+        parent: wrapper,
+        cnt: 2,
+    });
+
+    prevBtn.style.cssText = `
     position: absolute;
-    z-index:1;
+    z-index: 1;
     border: 0;
     top: 0;
     width: 50px;
     height: 100%;
     background-color: transparent;
-    background: linear-gradient(90deg, rgba(0, 0, 0, 0.3) 0%, rgba(0, 0, 0, 0.1) 30%, rgba(0, 0, 0, 0) 100%);
     filter: drop-shadow(3px 5px 2px rgba(0 0 0 / 0.7));
-`
-nextButton.style.background = `linear-gradient(270deg, rgba(0, 0, 0, 0.3) 0%, rgba(0, 0, 0, 0.1) 30%, rgba(0, 0, 0, 0) 100%)`;
+`;
+
+    nextBtn.style.cssText = prevBtn.style.cssText;
+
+
+    prevBtn.style.left = '0px';
+    nextBtn.style.right = '0px';
+
+
+    prevBtn.innerHTML = iconPrev;
+    nextBtn.innerHTML = iconNext;
+
+    nextBtn.onclick = () => {
+        handleSlide(true);
+    }
+
+    prevBtn.onclick = () => {
+        handleSlide(false);
+    }
+}
+function addImgItem(parent, src, cationText = 'Caption Text') {
+    const container = createEl({
+        tagName: 'div',
+        parent,
+    });
+
+    container.style.cssText = `
+        width: 400px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        overflow: hidden;
+    `;
+
+    const image = createEl({
+        tagName: 'img',
+        parent: container,
+        properties: { src }
+    });
+
+    image.style.cssText = `
+        width: 100%;
+        height: 100%;
+
+        }
+    `;
+
+    const caption = createEl({
+        tagName: 'span',
+        properties: { innerText: cationText },
+        parent: container,
+    });
+
+    caption.style.cssText = `
+        color: white;
+        font-weight: bold;
+        position: absolute;
+        filter: drop-shadow(3px 3px 30px rgba(0 0 0 / 0.5));
+    `;
+
+    return container;
+}
 
 function createEl({ tagName, properties, parent, children, cnt = 1 }) {
     const create = () => {
         const el = document.createElement(tagName);
-        Object.assign(el, properties); // 앞의 obj에 뒤 obj를 merge
+        Object.assign(el, properties);
         parent?.appendChild(el);
         children?.map(child => {
             child.parent = el;
